6931b3342442014184404c9f0096efbe2f1a294f
Nightbomd <antfel320@gmail.com>
2025-12-07 22:12:35 -0500

    finishing touches

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const questionOutput = document.getElementById("question-output")
 const eightBallImg = document.getElementById("ball-img")
 const responseImg = document.getElementById("response-img")
 
+// dont hack me okay
 const apiKey = "YlyDPUN0CDBb-5cJGK4PLItgFvdqUsGhSSsnmBorghE";
 function generateImg() {
     fetch(`https://api.unsplash.com/photos/random?client_id=${apiKey}`)
@@ -20,7 +21,7 @@ function generateImg() {
 function shakeMagic8Ball() {
     const question = prompt("Ask the Magic 8 Ball a question:");
     if (question.trim() === "") {
-        output.innerText = "Please type a question... idiot";
+        output.innerText = "Please enter a valid question.";
     }
     else {
         const responses = [
@@ -31,10 +32,23 @@ function shakeMagic8Ball() {
             "Better not tell you now.",
             "Don't count on it.",
             "My sources say no.",
+            "I'd assume so.",
+            "Absolutely!",
+            "The stars say yes.",
+            "A gallon of water could've prevented this btw.",
+            "The future is unclear.",
+            "Yes, definitely.",
+            "Outlook not so good.",
+            "Signs point to yes.",
+            "do you even lift bro?",
+            "do you get extra time on tests?",
+            "just give up atp",
+            "try asking google",
+            "maybe",
+            "no",
         ]
-      
         const randomIndex = Math.floor(Math.random() * responses.length);
-        questionOutput.innerHTML = `You asked: ${question}... The Magic 8 Ball says:`;
+        questionOutput.innerHTML = `You asked: <span>${question}...</span> The Magic 8 Ball says:`;
         output.innerText = responses[randomIndex];
         generateImg();
     }
